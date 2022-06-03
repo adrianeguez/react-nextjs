ed42ad5cf6f30697c18ffaccaf99432496063cb9
Adrian Eguez <eadepto@hotmail.com>
2022-06-02 19:21:38 -0500

    update/scss, tailwind

--- ejemplo-react/pages/index.tsx
+++ ejemplo-react/pages/index.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import React from "react";
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
     <p>
       <Link href="/about">
         <a>About</a>
